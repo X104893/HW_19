b4c9737972b5df057bb53d5700e718f4e4e35670
todideas@gmail.com <todideas@gmail>
2022-05-01 18:53:58 -0400

    removed images from Hw folder

--- fintech_finder.py
+++ fintech_finder.py
@@ -87,7 +87,7 @@ from crypto_wallet import generate_account, get_balance, send_transaction
 # Database of Fintech Finder candidates including their name, digital address, rating and hourly cost per Ether.
 # A single Ether is currently valued at $1,500
 candidate_database = {
-    "Lane": ["Lane", "0xaC8eB8B2ed5C4a0fC41a84Ee4950F417f67029F0", "4.3", .20, "Images/lane.jpeg"],
+    "Lane": ["Lane", "0x59f4545dBBF363046d002Dd26bf21f2b34837BCC", "4.3", .20, "Images/lane.jpeg"],
     "Ash": ["Ash", "0x2422858F9C4480c2724A309D58Ffd7Ac8bF65396", "5.0", .33, "Images/ash.jpeg"],
     "Jo": ["Jo", "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45", "4.7", .19, "Images/jo.jpeg"],
     "Kendall": ["Kendall", "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45", "4.1", .16, "Images/kendall.jpeg"]
@@ -269,6 +269,8 @@ if st.sidebar.button("Send Transaction"):
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
     # YOUR CODE HERE
+    st.markdown("this is account {}".format(account.address))
+    st.markdown("this is candidate {}".format(candidate_address))
     transaction_hash =  send_transaction(w3,account,candidate_address, wage)
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
